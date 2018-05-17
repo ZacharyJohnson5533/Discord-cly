e7dc43b7d384f190c0928b5f68848effe947f1c5
ZacharyJohnson5533 <pollothechicken93@gmail.com>
2018-05-16 17:26:59 -0700

    updates the updater/installer

--- update.sh
+++ update.sh
@@ -3,8 +3,8 @@ apt upgrade
 repoclone="https://github.com/ZacharyJohnson5533/Discord-cly.git"
 pip install --upgrade pip discord.py keyboard
 mkdir ~/Discord-clnt
+rm -rf ~/Discord-clnt
 cd ~/Discord-clnt
-rm *.*
 git clone --progress $repoclone .
 
 
